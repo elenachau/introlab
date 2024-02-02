692b9319b47c698c8dad4badd57bf6924325f78e
elenachau <elenaxcha@gmail.com>
2024-02-02 00:28:49 -0800

    create month attributes

--- dynamic-memory/month.h
+++ dynamic-memory/month.h
@@ -1,7 +1,31 @@
 #ifndef MONTH_H
 #define MONTH_H
 
+#include "day.h"
+
 class Month{
+    string name;
+    Day* firstMonth, *lastMonth;
+
+    public:
+        Month();
+        Month(string, Day*, Day*);
+        Month(const Month&);
+
+        void setName(string);
+        void setFirstOfMonth(Day*);
+        void setLastOfMonth(Day*);
+
+        string getName();
+        Day* getFirstOfMonth();
+        Day* getLastOfMonth();
+        
+        void addDayToMonth(Day);
+
+        Month& operator=(const Month&);
+        friend ostream& operator<<(ostream&, Month&);
+
+        ~Month();
 
 };
 
