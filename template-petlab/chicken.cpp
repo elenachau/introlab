38ad7fc4d3fb1fa8e5c9c4cf5d384b88e6ffb8eb
elenachau <elenaxcha@gmail.com>
2024-03-03 13:49:34 -0800

    create chicken.cpp class

---
+++ template-petlab/chicken.cpp
@@ -0,0 +1,53 @@
+#include "chicken.h"
+
+Chicken::Chicken():Pet(){ //specify base class Pet for inheritance
+    numEggs = -1;
+}
+
+Chicken::Chicken(const int num, const string nm, const string spec, const int ageVal, const Toy* toyArr, const int numT){
+    numEggs = num;
+    calculatePetCost();
+}
+
+Chicken::Chicken(const Chicken& rhs){
+    numEggs = rhs.numEggs;
+    name = rhs.name;
+    species = rhs.species;
+    age = rhs.age;
+    
+    for(int i = 0; i < rhs.numToys; i++){
+        ownedToys[i] = rhs.ownedToys[i];
+    }
+    numToys = rhs.numToys;
+}
+
+
+const int Chicken::getNumEggsLaid(){
+    return numEggs;
+}
+
+void Chicken::setNumEggsLaid(const int val){
+    numEggs = val;
+}
+
+void Chicken::printPetUtterance(){
+    cout << "\"Bawk\"";
+}
+
+void Chicken::calculatePetCost(){
+    if(numEggs == 0) {
+        cost = 3.55;
+    }
+    else {
+        cost = 3.55 + (.5*numEggs);
+    }
+
+    for(int i = 0; i < numToys; i++) {
+        cost += ownedToys[i].getCost();
+    }
+}
+
+const int Chicken::getIntValueFromChild(){
+    return numEggs;
+}
+
