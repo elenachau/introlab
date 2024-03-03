76bcb59e56b1984105a8de3d5490d41b48fea37c
elenachau <elenaxcha@gmail.com>
2024-03-03 13:50:30 -0800

    create petStore.h

---
+++ template-petlab/petStore.h
@@ -0,0 +1,24 @@
+#ifndef PETSTORE_H
+#define PETSTORE_H
+
+using namespace std;
+template <class T>
+
+class PetStore {
+    T **petStock; //a pointer to a pointer to type T
+    int num;
+
+    public:
+        PetStore();
+        PetStore(T*, int);
+        PetStore(const PetStore&);
+        ~PetStore();
+
+        T** getpetStock();
+        void addPetToStock(T*);
+
+        int getNum();
+};
+#include "petStore.cpp" //ensure template class is instantiated in .h, to prevent linker errors
+
+#endif
