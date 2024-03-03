687d8c18fd2096a90bc47c00eb009ffcce326bf2
elenachau <elenaxcha@gmail.com>
2024-03-03 14:22:44 -0800

    create main.cpp

--- template-petlab/main.cpp
+++ template-petlab/main.cpp
@@ -20,8 +20,30 @@ int main(){
     chickenToys[0] = xylophone;
     chickenToys[1] = mirror;
 
-    Cat Fluffernutter(7, "Fluffernutter", "Cat", 5, catToys, 1);
+    Cat Fluffernutter(7, "Fluffernutter", "Cat", 5, catToys, 1); //without instantiating pure virtual function, Cat would be considered abstract class and cannot be instantiated
     Chicken Hennifer(42, "Hennifer", "Chicken", 2, chickenToys, 2);
+
+    store->addPetToStock(&Fluffernutter); //& for an array of pointers
+    store->addPetToStock(&Hennifer);
+
+    for(int i = 0; i < 2; i++){
+        store->getPetStock()[i]->printPetData();
+    }
+
+    PetStore<Pet> *newStore = new PetStore<Pet>;
+
+    *newStore = *store; //performs deep copy of PetStore object that store is pointing to; newStore will point to copy PetStore obj
+
+    displayData("DUPLICATED SUCCESSFULLY");
+
+    for(int i = 0; i < 2; i++) {
+        newStore->getPetStock()[i]->printPetData();
+        displayData(i);
+    }
+
+    cout << endl;
+
+    return 0;
 }
 
 template<typename T>
