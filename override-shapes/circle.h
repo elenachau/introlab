550fb40b298ce2400e5617d3bc3e232f1fa9f6fe
elenachau <elenaxcha@gmail.com>
2024-03-12 20:19:20 -0700

    create circle.h

---
+++ override-shapes/circle.h
@@ -0,0 +1,22 @@
+#ifndef CIRCLE_H
+#define CIRCLE_H
+
+#include "shape.h"
+
+class Circle: public Shape{
+    float radius;
+    void calcPerimeter() override;
+    void calcArea() override;
+
+    public:
+        Circle();
+        Circle(float);
+        Circle(const Circle&);
+        virtual ~Circle();
+
+        float getRadius();
+        void setRadius(float);
+};
+
+
+#endif
