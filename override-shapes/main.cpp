eb50ed059a5579d79ea7890757553bc76ba158a4
elenachau <elenaxcha@gmail.com>
2024-03-14 21:05:52 -0700

    create main.cpp

---
+++ override-shapes/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+#include "triangle.h"
+#include "circle.h"
+#include "trapezoid.h"
+
+int main() {
+    cout << endl << "Triangle side lengths are 3, 4, 5: " << endl;
+    Triangle demoTriangle(3, 4, 5);
+    cout << "Area is " << demoTriangle.getArea() << endl;
+    cout << "Perimeter is " << demoTriangle.getPerimeter() << endl << endl;
+
+    cout << "Circle radius is 3: " << endl;
+    Circle demoCircle(3);
+    cout << "Area is " << demoCircle.getArea() << endl;
+    cout << "Perimeter is " << demoCircle.getPerimeter() << endl << endl;
+
+    cout << "Trapezoid base 1 is 12, base2 is 6, side 1 is 5, side 2 is 5, height is 4: " << endl;
+    Trapezoid demoTrapezoid(12, 6, 4, 5, 5);
+    cout << "Area is " << demoTrapezoid.getArea() << endl;
+    cout << "Perimeter is " << demoTrapezoid.getPerimeter() << endl << endl;
+
+    //Shape is an abstract base class, so an array of shape objects can't be created
+    //Instead create an array of pointers
+
+    Shape* shapeArray[50];
+
+    shapeArray[0] = &demoTriangle;
+    shapeArray[1] = &demoCircle;
+    shapeArray[2] = &demoTrapezoid;
+
+    for(int i = 0; i < 3; i++){
+        cout << shapeArray[i]->getArea() << endl;
+    }
+}
