af7c7ab4e91b20fa3cf332a2d8ccc82d5afde095
elenachau <elenaxcha@gmail.com>
2024-03-15 14:38:44 -0700

    create toy.h

---
+++ abstract-virtual-petlab/toy.h
@@ -0,0 +1,27 @@
+#ifndef TOY_H
+#define TOY_H
+
+#include <iostream>
+using namespace std;
+
+class Toy{
+    string toyName, brand;
+    float cost;
+
+    public:
+        Toy();
+        Toy(const string, const string, const float);
+        Toy(const Toy&);
+
+        string getToyName() const;
+        string getBrand() const;
+        float getCost() const;
+
+        void setToyName(const string);
+        void setBrand(const string);
+        void setCost(const float);
+
+        void displayToy();
+};
+
+#endif
